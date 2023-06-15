151e458ce8ab02cb1ed1b37d98ccc31548e84d87
Worlali <worlakodara13@gmail.com>
2023-06-15 15:44:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that outputs a line using printf
+ * Return: 0 (ZERO)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
