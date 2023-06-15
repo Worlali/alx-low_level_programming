7e9b86833de5a97f38a108fd12e6a695c89f214f
Worlali <worlakodara13@gmail.com>
2023-06-15 16:10:12 -0700

    6-sizes.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 	float f;
 printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %1u byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long int: %1u bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %1u bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %1u bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
