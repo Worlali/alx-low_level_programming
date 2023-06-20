5343ce5792848160e505fb79cc4c2133cc4c97ec
Worlali <worlakodara13@gmail.com>
2023-06-19 17:48:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+putchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
+putchar('\n');
+
+return (0);
+}
+
