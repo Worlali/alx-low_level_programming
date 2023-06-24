e934cd4c4a467731e6850226426adc59ae6f9754
Worlali <worlakodara13@gmail.com>
2023-06-23 19:48:54 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests fuction that prints if inter=ger is positive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+	i=0;
+	positive_ornegative(i);
+
+	return (0);
+}
