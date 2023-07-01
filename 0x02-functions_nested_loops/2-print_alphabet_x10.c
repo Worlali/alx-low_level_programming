b0de4ffe6f2edb384a5c1b05894597987e9d75ba
Worlali <worlakodara13@gmail.com>
2023-07-01 08:19:42 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  * Return: void
  */
 
-void prit_alphabet_10(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
 	i = 0;
 	while (i < 10)
 	{
-		for (ch ='a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
